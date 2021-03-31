40eed3c69241ff540b5f30914000d2c4724e6bf3
alinasol-source <alinabrr1@yandex.ru>
2021-03-31 10:37:58 +0300

    second

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import revers
 
 class City(models.Model):
     name = models.CharField(max_length=30)
--- helloworld/urls.py
+++ helloworld/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('delete/<city_name>/', views.delete_city, name='delete_city')
+    path('delete/<city_name>/', views.delete_city, name='delete_city'),
+    path('login', views.log_in, name="login"),
+    path('logout', views.log_out, name="logout"),
+    path('signup', views.sign_up, name="signup"),
 ]
